79000c09e8bc3e369bbc0258f20ad33ea727c75e
Mohammed-salah-iq <mo7ammed.salah@outlook.com>
2022-01-16 18:30:10 +0300

    simple react project

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,11 @@
+import logo from "../images/react_logo.png";
+
 export default function Navebar() {
-    return (
-        <h1>Navbar goes here</h1>
-    )
+  return (
+    <nav>
+      <img src={logo} className="nav--icon" />
+      <h3 className="nav--logo_text">ReactFacts</h3>
+      <h4 className="nav--title">React Course - Project 1</h4>
+    </nav>
+  );
 }
